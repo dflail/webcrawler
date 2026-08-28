289a4e513aa215b19ed537f13b49561bf45bf645
dflail <dan.flail.dev@gmail.com>
2026-08-28 08:25:52 -0400

    Updated normalization flow and unit tests

--- test_crawl.py
+++ test_crawl.py
@@ -6,8 +6,32 @@ class TestCrawl(unittest.TestCase):
         url = "https://www.boot.dev/blog/path"
         expected = "www.boot.dev/blog/path"
         result = normalize_url(url)
-        print(f"Testing normalize_url with input: {url}")
         self.assertEqual(result, expected)
+
+    def test_normalize_url_port(self):
+        input_url = "https://boot.dev:8080/path/"
+        actual = normalize_url(input_url)
+        expected = "boot.dev/path"
+        self.assertEqual(actual, expected)
+
+    def test_normalize_url_trailing_slash(self):
+        input_url = "https://boot.dev/path/"
+        actual = normalize_url(input_url)
+        expected = "boot.dev/path"
+        self.assertEqual(actual, expected)
+
+    def test_normalize_url_capitals(self):
+        input_url = "https://BOOT.DEV/path"
+        actual = normalize_url(input_url)
+        expected = "boot.dev/path"
+        self.assertEqual(actual, expected)
+
+    def test_normalize_url_root_slash(self):
+        input_url = "https://boot.dev/"
+        actual = normalize_url(input_url)
+        expected = "boot.dev"
+        self.assertEqual(actual, expected)
+
 
     if __name__ == "__main__":
         unittest.main()
